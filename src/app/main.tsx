749d99fe09f44652df2dd89e247efb3a71393e24
TM-RazyCraft <ts_pt-tanaka>
2026-02-24 17:58:46 +0900

    疑似BASIC認証

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -5,8 +5,23 @@ import App from "./App";
 import { StrictMode } from "react";
 import "@/styles/index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+if (location.hostname === "localhost") {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+else {
+  const hash = import.meta.env.VITE_HASH;
+  const userPassword = prompt("パスワードを入力してください");
+  if (userPassword === hash) {
+    createRoot(document.getElementById("root")!).render(
+      <StrictMode>
+        <App />
+      </StrictMode>
+    );
+  } else {
+    alert("パスワードが間違っています");
+  }
+}
